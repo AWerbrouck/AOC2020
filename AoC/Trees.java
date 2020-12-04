a72251a51edc92193a6d3a4462caff01a9d6323f
AWerbrouck <arthur.jasaw@gmail.com>
2020-12-04 15:11:37 +0100

    DONE

--- AoC/Trees.java
+++ AoC/Trees.java
@@ -1,6 +1,6 @@
 package AoC;
 
-import java.util.*;
+import java.util.List;
 
 public class Trees {
 	public static void main(String[] args) {
@@ -9,16 +9,14 @@ public class Trees {
 	
 	public static int dag3(int down, int right) {
 		int trees = 0;
-		int H= 0;
+		int H = 0;
 		List<String> list = ReadPuzzle.readPuzzle("d3.txt");
 		int len = (list.get(0)).length();
-		for (int i = 1; i < list.size(); i += down) {
+		for (int i = down; i < list.size(); i += down) {
 			H = (H + right) % len;
-			if (list.get(i).charAt(H) == '#'){
+			if (list.get(i).charAt(H) == '#') {
 				trees++;
-				
 			}
-			
 		}
 		return trees;
 	}
