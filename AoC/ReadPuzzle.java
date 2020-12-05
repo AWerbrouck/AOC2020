73dd472d26499288deeeb9da42aab4df836b1a2a
awerbrouck <arthur.jasaw@gmail.com>
2020-12-05 23:49:41 +0100

    done so far yeet

--- AoC/ReadPuzzle.java
+++ AoC/ReadPuzzle.java
@@ -2,6 +2,10 @@ package AoC;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -25,5 +29,24 @@ public class ReadPuzzle {
             exception.printStackTrace();
         }
         return null;
+    }
+
+    public static int[] readPuzzleInt(String fileName) throws FileNotFoundException {
+        int[] intArray = new int[]{};
+        Scanner scanner = new Scanner(new File("./puzzels/" + fileName));
+        int i = 0;
+        while(scanner.hasNextInt()){
+            intArray[i++] = scanner.nextInt();
+         }
+        return intArray;
+
+    }
+    public static String[] getInput(String fileName) throws FileNotFoundException{
+        try {
+            return Files.lines(Path.of(fileName)).toArray(String[]::new);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new String[0];
+        }
     }
 }
--- AoC/Trees.java
+++ AoC/Trees.java
@@ -8,35 +8,14 @@ public class Trees {
     }
 
     public static int dag3(int down, int right) {
-        int skip = 0;
         int trees = 0;
-        int indexHorizontal = 0;
+        int x = 0;
         List<String> list = ReadPuzzle.readPuzzle("d3.txt");
         int len = (list.get(0)).length();
+        char[][] charar = [][];
         for (String string : list) {
-            if (skip == 1) {
-                char[] chararray = string.toCharArray();
-                if (indexHorizontal <= len - right - 1) {
-                    indexHorizontal = indexHorizontal + right;
-                    char obj = chararray[indexHorizontal];
-                    if (obj == '#') {
-                        trees++;
-                    }
 
-                } else {
-                    indexHorizontal = ((indexHorizontal + right) % len);
-                    char obj = chararray[indexHorizontal];
-                    if (obj == '#') {
-                        trees++;
-                    }
-                }
-                if (down == 2) {
-                    skip = 0;
-                }
-            } else {
-                skip = 1;
-            }
-        }
+            
         return trees;
     }
 }
