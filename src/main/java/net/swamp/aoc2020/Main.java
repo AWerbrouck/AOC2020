5f331d2773318bf5952811b4ee27acfce02a9c8e
AWerbrouck <Arthur.jasaw@gmail.com>
2020-12-09 22:33:47 +0100

    prepping d10

--- src/main/java/net/swamp/aoc2020/Main.java
+++ src/main/java/net/swamp/aoc2020/Main.java
@@ -5,15 +5,16 @@ import java.util.List;
 public class Main {
 	private static final PuzzleInputFetcher PUZZLE_INPUT_FETCHER = new PuzzleInputFetcher();
 	private static final List<AbstractPuzzle> PUZZLES = List.of(
-			new Puzzle01(PUZZLE_INPUT_FETCHER.getPuzzleInput(1)),
-			new Puzzle02(PUZZLE_INPUT_FETCHER.getPuzzleInput(2)),
-			new Puzzle03(PUZZLE_INPUT_FETCHER.getPuzzleInput(3)),
-			new Puzzle04(PUZZLE_INPUT_FETCHER.getPuzzleInput(4)),
-			new Puzzle05(PUZZLE_INPUT_FETCHER.getPuzzleInput(5)),
-			new Puzzle06(PUZZLE_INPUT_FETCHER.getPuzzleInput(6)),
-			new Puzzle07(PUZZLE_INPUT_FETCHER.getPuzzleInput(7)),
-			new Puzzle08(PUZZLE_INPUT_FETCHER.getPuzzleInput(8)),
-			new Puzzle09(PUZZLE_INPUT_FETCHER.getPuzzleInput(9))
+//			new Puzzle01(PUZZLE_INPUT_FETCHER.getPuzzleInput(1)),
+//			new Puzzle02(PUZZLE_INPUT_FETCHER.getPuzzleInput(2)),
+//			new Puzzle03(PUZZLE_INPUT_FETCHER.getPuzzleInput(3)),
+//			new Puzzle04(PUZZLE_INPUT_FETCHER.getPuzzleInput(4)),
+//			new Puzzle05(PUZZLE_INPUT_FETCHER.getPuzzleInput(5)),
+//			new Puzzle06(PUZZLE_INPUT_FETCHER.getPuzzleInput(6)),
+//			new Puzzle07(PUZZLE_INPUT_FETCHER.getPuzzleInput(7)),
+//			new Puzzle08(PUZZLE_INPUT_FETCHER.getPuzzleInput(8)),
+//			new Puzzle09(PUZZLE_INPUT_FETCHER.getPuzzleInput(9)),
+			new Puzzle10(PUZZLE_INPUT_FETCHER.getPuzzleInput(10))
 	);
 	
 	public static void main(String[] args) {
---
+++ src/main/java/net/swamp/aoc2020/Puzzle10.java
@@ -0,0 +1,24 @@
+package net.swamp.aoc2020;
+
+public class Puzzle10 extends AbstractPuzzle{
+	public Puzzle10(String puzzleInput) {
+		super(puzzleInput);
+	}
+	
+	@Override
+	public int getDay() {
+		return 10;
+	}
+	
+	@Override
+	public String solvePart1() {
+		return null;
+	}
+	
+	@Override
+	public String solvePart2(){
+		
+		return null;
+	}
+	
+}
